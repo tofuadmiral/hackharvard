ed4d579ba3c572224d1db35f482ccc99db225866
tofuadmiral <fuad98ali@gmail.com>
2018-10-20 23:23:28 -0400

    added payment function

---
+++ testpayment.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const ilp = require('ilp')
+const spsp = require('ilp-protocol-spsp')
+const debug = require('debug')('ilp-spsp')
+
+// recipient is the payment pointer
+// amount is 1 XRP = 10^9 units
+async function pay (recipient, amount) {
+  try {
+    const plugin = ilp.createPlugin()
+    debug('connecting plugin')
+    await plugin.connect()
+
+    debug('sending payment')
+    await spsp.pay(plugin, {
+      receiver: recipient,
+      sourceAmount: amount
+    })
+  } catch (e) {
+    console.error(e)
+    process.exit(1)
+  }
+
+  console.log('sent!')
+  process.exit(0)
+}
+
+// sending 0.0000001 XRP
+function run () {
+  pay('$spsp.strata-ilsp-3.com:8084', 100)
+}
+
+run()
